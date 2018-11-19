64aed38d36cf75cc267d54e8da73d54a7dd809b1
mattkinloch <mattkinloch1@gmail.com>
2018-11-19 15:50:44 -0500

    cleaned up code + comments

--- Assignment 1/js/main.js
+++ Assignment 1/js/main.js
@@ -1,17 +1,14 @@
 function createDiv()
 {
-	// Create Div
+	// Create Left Div
 	var leftDiv = document.createElement('div');
-	var rightDiv = document.createElement('div');
-
-	leftDiv.className = 'lDiv';
 	leftDiv.id = 'theLeftDiv';
-	leftDiv.setAttribute('title', 'Left Div');
 
-	rightDiv.className = 'rDiv';
+	//Create Right Div
+	var rightDiv = document.createElement('div');
 	rightDiv.id = 'theRightDiv';
-	rightDiv.setAttribute('title', 'Right Div');
 
+	//Create Left Div text
 	var leftDivText = document.createTextNode("Select a Number ");
 	leftDivText.className = "lDivText";
 	leftDiv.appendChild(leftDivText);
@@ -30,6 +27,7 @@ function createDiv()
 	// Create Event Listener
 	select.addEventListener("change", function(){
 
+		// If Right Div has any children, remove them
 		if(rightDiv.hasChildNodes())
 		{
 			while(rightDiv.firstChild)
@@ -38,37 +36,46 @@ function createDiv()
 			}
 		}
 		
+		// Get the text of the option chosen, convert to int, and get the animal at that index
 		var index = parseInt(select.options[select.selectedIndex].textContent) - 1;
+
+		// Append the information about the chosen animal to the right div
 		rightDivText = document.createTextNode((animalsList[index]).describe());
 		rightDiv.appendChild(rightDivText);
 
 	});
 		
-	// Append Select Box to Div, Div to Body
+	// Append Select Box to Left Div
 	leftDiv.appendChild(select);
+
+	// Append Left Div and Right Div to body
 	document.body.appendChild(leftDiv);
 	document.body.appendChild(rightDiv);
 }
 
+// Animal Object
 function Animal(theName, theDescription)
 {
 	this.name = theName;
 	this.description = theDescription;
 
 	this.describe = function(){
-		return "Name: " + this.name + " Description: " + this.description;
+		return "Name: " + this.name + "\n" + "Description: " + this.description;
 	}
 }
 
-var dog = new Animal("Dog (Canis lupus familiaris)", "The domestic dog is a member of the genus Canis, which forms part of the wolf-like canids, and is the most widely abundant terrestrial carnivore.");
-var cat = new Animal("Cat (Felis catus)", "The cat, often referred to as the domestic cat, is a small, typically furry, carnivorous mammal.");
-var goat = new Animal("Goat (Caora aegagrus aegagrus)", "The goat is a member of the animal family Bovidae and the goat-antelope subfamily Caprinae, meaning it is closely related to the sheep.");
-var sheep = new Animal("Sheep (Ovis aries)", "Domestic sheep are quadrupedal, ruminant mammals typically kept as livestock. Like most ruminants, sheep are members of the order Artiodactyla, the even-toed ungulates.");
-var chicken = new Animal("Chicken (Gallus gallus domesticus)", "The chicken is a type of domesticated fowl, a subspecies of the red junglefowl. It is one of the most common and widespread domestic animals, with a total population of more than 19 billion as of 2011.");
-var donkey = new Animal("Donkey (Equus africanus asinus)", "The donkey or ass is a domesticated member of the horse family, Equidae. The wild ancestor of the donkey is the African wild ass, E. africanus. The donkey has been used as a working animal for at least 5000 years.");
-var duck = new Animal("Duck (Anas platyrhynchos domesticus)", "Duck is the common name for a large number of species in the waterfowl family Anatidae which also includes swans and geese. Ducks are divided among several subfamilies in the family Anatidae.");
-var bee = new Animal("Honey Bee (Apis melifera)", "Bees are flying insects closely related to wasps and ants, known for their role in pollination and, in the case of the best-known bee species, the western honey bee, for producing honey and beeswax.");
-var horse = new Animal("Horse (Equus ferus caballus)", "The horse is one of two extant subspecies of Equus ferus. It is an odd-toed ungulate mammal belonging to the taxonomic family Equidae. The horse has evolved over the past 45 to 55 million years from a small multi-toed creature, Eohippus, into the large, single-toed animal of today.");
-var goldfish = new Animal("Goldfish (Carassius auratus)", "The goldfish is a freshwater fish in the family Cyprinidae of order Cypriniformes. It is one of the most commonly kept aquarium fish. A relatively small member of the carp family, the goldfish is native to East Asia.");
+// Animals, info from https://en.wikipedia.org/wiki/List_of_domesticated_animals
+var dog = new Animal("Dog", "The domestic dog is a member of the genus Canis, which forms part of the wolf-like canids, and is the most widely abundant terrestrial carnivore.");
+var cat = new Animal("Cat", "The cat, often referred to as the domestic cat, is a small, typically furry, carnivorous mammal.");
+var goat = new Animal("Goat", "The goat is a member of the animal family Bovidae and the goat-antelope subfamily Caprinae, meaning it is closely related to the sheep.");
+var sheep = new Animal("Sheep", "Domestic sheep are quadrupedal, ruminant mammals typically kept as livestock. Like most ruminants, sheep are members of the order Artiodactyla, the even-toed ungulates.");
+var chicken = new Animal("Chicken", "The chicken is a type of domesticated fowl, a subspecies of the red junglefowl. It is one of the most common and widespread domestic animals, with a total population of more than 19 billion as of 2011.");
+var donkey = new Animal("Donkey", "The donkey is a domesticated member of the horse family, Equidae. The wild ancestor of the donkey is the African wild ass, E. africanus. The donkey has been used as a working animal for at least 5000 years.");
+var duck = new Animal("Duck", "Duck is the common name for a large number of species in the waterfowl family Anatidae which also includes swans and geese. Ducks are divided among several subfamilies in the family Anatidae.");
+var bee = new Animal("Honey Bee", "Bees are flying insects closely related to wasps and ants, known for their role in pollination and, in the case of the best-known bee species, the western honey bee, for producing honey and beeswax.");
+var horse = new Animal("Horse", "The horse is one of two extant subspecies of Equus ferus. It is an odd-toed ungulate mammal belonging to the taxonomic family Equidae. The horse has evolved over the past 45 to 55 million years from a small multi-toed creature, Eohippus, into the large, single-toed animal of today.");
+var goldfish = new Animal("Goldfish", "The goldfish is a freshwater fish in the family Cyprinidae of order Cypriniformes. It is one of the most commonly kept aquarium fish. A relatively small member of the carp family, the goldfish is native to East Asia.");
 
+
+// List of Animals
 var animalsList = [dog, cat, goat, sheep, chicken, donkey, duck, bee, horse, goldfish];
